18d43007cb16882276523866cf2f1e1853dd664e
Lanways <mcbluedd@gmail.com>
2023-03-12 01:51:24 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const Link = require('./models/link')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
@@ -8,16 +6,9 @@ const routes = require('./routes')
 if (process.env.NODE_ENW !== 'production') {
   require('dotenv').config()
 }
-mongoose.connect(process.env.MOGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+require('./config/mongoose')
 const app = express()
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -25,6 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(3000, () => {
-  console.log('Express is running on http://localhost3000')
+  console.log(`Express is running on http://localhost:${PORT}`)
 })
 
